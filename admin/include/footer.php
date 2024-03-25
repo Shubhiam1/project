fe6b2a7c31d71ac0cb877d92894ec63a4133a632
Shubhiam1 <bhardwajshubham041@gmail.com>
2024-03-25 17:16:59 +0530

    Initial commit

---
+++ admin/include/footer.php
@@ -0,0 +1,10 @@
+ <footer class="footer" style="text-align: center;">Shubham Bhardwaj © 2023 - Online Food Ordering System Developer - <a href="https://www.instagram.com/_01_shubham__/">Shubham Bhardwaj</a> </footer>
+ <!--/*!
+ * Author Name: MH RONY.
+ * GigHub Link: https://github.com/dev-mhrony
+ * Facebook Link:https://www.facebook.com/dev.mhrony
+ * Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
+ for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com
+ * Visit My Website : developerrony.com
+
+ */ -->
